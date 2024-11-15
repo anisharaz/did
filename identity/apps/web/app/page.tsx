6a32ce6aa6aeff8213325d6659549ac64710cfa1
Manish Lal <manishlal2274@gmail.com>
2024-11-15 10:05:22 +0530

    Added home page and navbar

--- identity/apps/web/app/page.tsx
+++ identity/apps/web/app/page.tsx
@@ -1,15 +1,55 @@
-import WalletButton from "@/components/ui/WalletButton";
+import AppBar from "@/components/ui/AppBar";
+import Image from "next/image";
 
 function Home() {
   return (
-    <div className="min-h-16 shadow-lg flex items-center">
-      <div className="flex-1">
-        <div>LOGO</div>
+    <main className="bg-[url('https://static.aaraz.me/did/bg.jpeg')] bg-cover min-h-screen font-mono">
+      <AppBar />
+      {/* text description */}
+      <div className=" pt-64 pl-36">
+        <div>
+          <h1
+            className=" text-9xl font-extrabold"
+            style={{
+              fontFamily: "Round Digitalio Expanded",
+              fontWeight: "100",
+              color: "yellow",
+              textShadow: "0px 0px 20px rgb(255, 16, 240)",
+            }}
+          >
+            Decentralized Identification(DID)
+          </h1>
+        </div>
+        <p className="text-[#EEEEEE] text-xl mt-4 ml-4">
+          This is a decentralised way to store data and prevent unauthorised
+          access and hence prevent tampering of dapinkta
+        </p>
       </div>
-      <div>
-        <WalletButton />
-      </div>
-    </div>
+      <footer>
+        <div></div>
+        <div>
+          <form className="flex flex-col max-w-[450px] gap-2 ml-4 p-4">
+            <h1 className="text-white text-xl font-bold">
+              Submit your queries
+            </h1>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className="mb-4 px-2 py-2"
+            />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message..."
+              className="py-8"
+            ></textarea>
+            <button className="bg-[#16213E] py-2 text-white rounded-md mt-1 mx-24">
+              Submit
+            </button>
+          </form>
+        </div>
+      </footer>
+    </main>
   );
 }
 
--- identity/apps/web/components/ui/AppBar.tsx
+++ identity/apps/web/components/ui/AppBar.tsx
@@ -1,6 +1,49 @@
 "use client";
+import WalletButton from "./WalletButton";
+import { FaGithub } from "react-icons/fa";
+import logo from "../../public/logo.png";
+import Image from "next/image";
+
 function AppBar() {
-  return <div></div>;
+  return (
+    <nav className="flex items-center border border-3 border-black py-6 min-w-full place-content-between pt-10 text-white">
+      <div className="ml-10">
+        <Image
+          src={logo}
+          alt="logo"
+          height={5}
+          width={100}
+          className="h-[50px] w-[100px] rounded-md"
+        />
+      </div>
+      <div className="flex gap-20">
+        <div>
+          <a href="/">Home</a>
+        </div>
+        <div>
+          <a href="#how-it-works">How it works?</a>
+        </div>
+        <div>
+          <a href="#footer">Contact Us</a>
+        </div>
+        <div className="text-2xl">
+          {/* github link */}
+          <a href="">
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+
+      <div className="mr-20 flex gap-16">
+        <a href="/dashboard" className="bg-black text-white rounded-md p-3">
+          Dashboard
+        </a>
+
+        {/* wsllet button link */}
+        <a href="">{/* <WalletButton /> */}</a>
+      </div>
+    </nav>
+  );
 }
 
 export default AppBar;
