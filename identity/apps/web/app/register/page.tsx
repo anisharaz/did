77c4af5d0585abf894170b5e798dbdbf6838a0b0
Anish Araz <anisharaz919@gmail.com>
2024-11-15 13:07:51 +0530

    UPDATE: 'create people' form UI

--- identity/apps/web/app/register/page.tsx
+++ identity/apps/web/app/register/page.tsx
@@ -69,190 +69,211 @@ export default function RegisterForm() {
   // TODO: add the id in all input for Label
   // TODO: ability to upload image
   return (
-    <div className="grid grid-cols-2 h-screen bg-slate-100">
-      <div className="flex justify-center items-center">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <Label htmlFor="firstName">
-              First Name<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              id="firstName"
-              {...register("firstName")}
-              placeholder="Enter your first name"
-            />
-            {errors.firstName && (
-              <p className="text-red-500">{errors.firstName.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="middleName">Middle Name (Optional)</Label>
-            <Input
-              id="middleName"
-              {...register("middleName")}
-              placeholder="Enter your middle name"
-            />
-            {errors.middleName && (
-              <p className="text-red-500">{errors.middleName.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="lastName">
-              Last Name<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              id="lastName"
-              {...register("lastName")}
-              placeholder="Enter your last name"
-            />
-            {errors.lastName && (
-              <p className="font-light text-red-600">
-                {errors.lastName.message}
-              </p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="phone">
-              Phone (with country code):
-              <span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              id="phone"
-              {...register("phone")}
-              placeholder="Mobile number"
-            />
-            {errors.phone && (
-              <p className="text-red-500">{errors.phone.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="email">
-              Email<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              id="email"
-              {...register("email")}
-              placeholder="Enter email address"
-            />
-            {errors.email && (
-              <p className="text-red-500">{errors.email.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="address">
-              Address Line<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input type="text" {...register("address_line")} />
-            {errors.address_line && (
-              <p className="text-red-500">{errors.address_line.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="address">
-              State<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input type="text" {...register("state")} />
-            {errors.state && (
-              <p className="text-red-500">{errors.state.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="address">
-              City<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input type="text" {...register("city")} />
-            {errors.city && (
-              <p className="text-red-500">{errors.city.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="address">
-              Pin Code<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              type="text"
-              {...register("pin_code", {
-                valueAsNumber: true,
-              })}
-            />
-            {errors.pin_code && (
-              <p className="text-red-500">{errors.pin_code.message}</p>
-            )}
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="gender">
-              Gender<span className="font-light text-red-600">*</span>
-            </Label>
-            <Controller
-              name="gender"
-              control={control}
-              render={({ field: { onChange, onBlur, value, ref } }) => (
-                <RadioGroup
-                  onValueChange={(e) => {
-                    setValue("gender", e);
-                    clearErrors("gender");
-                  }}
-                  value={value}
-                >
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="male" id="male" />
-                    <Label htmlFor="male">Male</Label>
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="female" id="female" />
-                    <Label htmlFor="female">Female</Label>
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="other" id="other" />
-                    <Label htmlFor="other">Other</Label>
-                  </div>
-                </RadioGroup>
+    <div className="grid grid-cols-2 text-white h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="">
+        <div>
+          <div className="text-4xl font-bold p-4">Enter People detail</div>
+          <hr />
+          <form onSubmit={handleSubmit(onSubmit)} className="p-2">
+            <div className="grid grid-cols-3 gap-2 p-2">
+              <div>
+                <Label htmlFor="firstName" className="text-xl">
+                  First Name<span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  id="firstName"
+                  {...register("firstName")}
+                  placeholder="Enter your first name"
+                />
+                {errors.firstName && (
+                  <p className="text-red-500">{errors.firstName.message}</p>
+                )}
+              </div>
+              <div>
+                <Label className="text-xl" htmlFor="middleName">
+                  Middle Name (Optional)
+                </Label>
+                <Input
+                  id="middleName"
+                  {...register("middleName")}
+                  placeholder="Enter your middle name"
+                />
+                {errors.middleName && (
+                  <p className="text-red-500">{errors.middleName.message}</p>
+                )}
+              </div>
+              <div>
+                <Label className="text-xl" htmlFor="lastName">
+                  Last Name<span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  id="lastName"
+                  {...register("lastName")}
+                  placeholder="Enter your last name"
+                />
+                {errors.lastName && (
+                  <p className="font-light text-red-600">
+                    {errors.lastName.message}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-2 p-2">
+              <div>
+                <Label className="text-xl" htmlFor="phone">
+                  Phone (with country code):
+                  <span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  id="phone"
+                  {...register("phone")}
+                  placeholder="Mobile number"
+                />
+                {errors.phone && (
+                  <p className="text-red-500">{errors.phone.message}</p>
+                )}
+              </div>
+              <div>
+                <Label className="text-xl" htmlFor="email">
+                  Email<span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  id="email"
+                  {...register("email")}
+                  placeholder="Enter email address"
+                />
+                {errors.email && (
+                  <p className="text-red-500">{errors.email.message}</p>
+                )}
+              </div>
+            </div>
+            <div className="p-2">
+              <Label className="text-xl" htmlFor="address">
+                Address Line<span className="font-light text-red-600">*</span>
+              </Label>
+              <Input type="text" {...register("address_line")} />
+              {errors.address_line && (
+                <p className="text-red-500">{errors.address_line.message}</p>
               )}
-            ></Controller>
-            {errors.gender && (
-              <p className="text-red-500">{errors.gender.message}</p>
-            )}
-          </div>
-          <div>
-            <Label htmlFor="photo">
-              Upload a photo<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              type="file"
-              onChange={(e) => {
-                const file = e.target.files?.[0];
-                if (file) {
-                  setFile(file);
-                  setValue("photo", URL.createObjectURL(file));
-                }
-              }}
-            />
-          </div>
-          <div>
-            <Label htmlFor="dob">
-              Date Of Birth<span className="font-light text-red-600">*</span>
-            </Label>
-            <Input
-              type="date"
-              {...register("dob", {
-                valueAsDate: true,
-              })}
-              placeholder="Select your date of birth"
-            />
-            {errors.dob && <p className="text-red-500">{errors.dob.message}</p>}
-          </div>
-          <Button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? (
-              <>
-                <Loader2 className="animate-spin" />{" "}
-                <span className="pl-2">Please Wait..</span>
-              </>
-            ) : (
-              <>Submit</>
-            )}
-          </Button>
-        </form>
-        {errors.root && <p className="text-red-500">Submission Error</p>}
+            </div>
+            <div className="grid grid-cols-3 gap-2 p-2">
+              <div>
+                <Label className="text-xl" htmlFor="address">
+                  State<span className="font-light text-red-600">*</span>
+                </Label>
+                <Input type="text" {...register("state")} />
+                {errors.state && (
+                  <p className="text-red-500">{errors.state.message}</p>
+                )}
+              </div>
+              <div>
+                <Label className="text-xl" htmlFor="address">
+                  City<span className="font-light text-red-600">*</span>
+                </Label>
+                <Input type="text" {...register("city")} />
+                {errors.city && (
+                  <p className="text-red-500">{errors.city.message}</p>
+                )}
+              </div>
+              <div>
+                <Label className="text-xl" htmlFor="address">
+                  Pin Code<span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  type="text"
+                  {...register("pin_code", {
+                    valueAsNumber: true,
+                  })}
+                />
+                {errors.pin_code && (
+                  <p className="text-red-500">{errors.pin_code.message}</p>
+                )}
+              </div>
+            </div>
+            <div className="space-y-2 p-2">
+              <Label className="text-xl" htmlFor="gender">
+                Gender<span className="font-light text-red-600">*</span>
+              </Label>
+              <Controller
+                name="gender"
+                control={control}
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                  <RadioGroup
+                    onValueChange={(e) => {
+                      setValue("gender", e);
+                      clearErrors("gender");
+                    }}
+                    value={value}
+                  >
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="male" id="male" />
+                      <Label htmlFor="male">Male</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="female" id="female" />
+                      <Label htmlFor="female">Female</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="other" id="other" />
+                      <Label htmlFor="other">Other</Label>
+                    </div>
+                  </RadioGroup>
+                )}
+              ></Controller>
+              {errors.gender && (
+                <p className="text-red-500">{errors.gender.message}</p>
+              )}
+            </div>
+            <div className="grid grid-cols-2 gap-2 p-2">
+              <div>
+                <Label className="text-xl" htmlFor="photo">
+                  Upload a photo
+                  <span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  type="file"
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    if (file) {
+                      setFile(file);
+                      setValue("photo", URL.createObjectURL(file));
+                    }
+                  }}
+                />
+              </div>
+              <div>
+                <Label className="text-xl" htmlFor="dob">
+                  Date Of Birth
+                  <span className="font-light text-red-600">*</span>
+                </Label>
+                <Input
+                  type="date"
+                  {...register("dob", {
+                    valueAsDate: true,
+                  })}
+                  placeholder="Select your date of birth"
+                  className="text-black"
+                />
+                {errors.dob && (
+                  <p className="text-red-500">{errors.dob.message}</p>
+                )}
+              </div>
+            </div>
+            <div className="p-2">
+              <Button type="submit" disabled={isSubmitting}>
+                {isSubmitting ? (
+                  <>
+                    <Loader2 className="animate-spin" />{" "}
+                    <span className="pl-2">Please Wait..</span>
+                  </>
+                ) : (
+                  <>Submit</>
+                )}
+              </Button>
+            </div>
+          </form>
+          {errors.root && <p className="text-red-500">Submission Error</p>}
+        </div>
       </div>
       <div className="flex justify-center items-center bg-white">
         <Image
